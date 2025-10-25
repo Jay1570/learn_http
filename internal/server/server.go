dfe5201e778f05fdab0d89bbf2d2b4a0f741d8c5
Jay1570 <jaygheewala157@gmail.com>
2025-10-25 11:02:42 +0530

    Implement initial server and response handling; add methods for writing status lines and headers

---
+++ internal/server/server.go
@@ -0,0 +1,58 @@
+package server
+
+import (
+	"fmt"
+	"io"
+	"learn_http/internal/response"
+	"net"
+)
+
+type Server struct {
+	closed bool
+}
+
+func runConnection(s *Server, conn io.ReadWriteCloser) {
+	defer conn.Close()
+
+	headers := response.GetDefaultHeaders(0)
+
+	response.WriteStatusLine(conn, response.StatusOK)
+	response.WriteHeaders(conn, headers)
+}
+
+func runServer(s *Server, listener net.Listener) {
+	for {
+		conn, err := listener.Accept()
+
+		if s.closed {
+			return
+		}
+
+		if err != nil {
+			return
+		}
+
+		go runConnection(s, conn)
+	}
+}
+
+func Serve(port uint16) (*Server, error) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+	server := &Server{closed: false}
+
+	go runServer(server, listener)
+
+	return server, nil
+}
+
+func (s *Server) Close() error {
+	s.closed = true
+	return nil
+}
+
+func (s *Server) listen() {}
+
+func (s *Server) handle(conn net.Conn) {}
